5db44942609e24f5fbe735fbf320c3ee9b29c2ab
John <johnys@cs.stanford.edu>
2020-11-28 16:19:13 -0500

    fix key issue for rvm

--- linux/install_tools.sh
+++ linux/install_tools.sh
@@ -134,6 +134,11 @@ nvm install 0.10
 source ~/.bash_profile
 
 # Ruby version manager rvm (https://github.com/rvm/ubuntu_rvm)
+
+# Public Key Fix
+gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+
+# Install RVM
 sudo apt-get install software-properties-common
 sudo apt-add-repository -y ppa:rael-gc/rvm
 sudo apt-get update
